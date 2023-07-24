f662619760d801d095186826203c8dfb43fbc91a
Esref <csresrf@gmail.com>
2023-07-24 02:15:37 +0200

    24.07.23Ec

---
+++ src/test/java/com/euroTech/stepDefs/US02_LogoutFunctionality_Step_Defs.java
@@ -0,0 +1,72 @@
+package com.euroTech.stepDefs;
+
+import com.euroTech.pages.HomePage;
+import com.euroTech.pages.LoginPage;
+import com.euroTech.pages.LogoutPage;
+import com.euroTech.utilities.BrowserUtils;
+import com.euroTech.utilities.ConfigurationReader;
+import com.euroTech.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import static com.euroTech.utilities.Driver.driver;
+
+
+public class US02_LogoutFunctionality_Step_Defs {
+
+
+    HomePage homePage = new HomePage();
+
+    LoginPage loginPage = new LoginPage();
+    LogoutPage logoutPage = new LogoutPage();
+
+    Actions actions = new Actions(driver);
+
+    @Then("The user cliks to My Account dropdown menu and Logout button")
+    public void the_user_cliks_to_my_account_dropdown_menu_and_logout_button() throws InterruptedException {
+        logoutPage.myAccountDDM.click();
+        logoutPage.logoutDDM.click();
+        Thread.sleep(3000);
+    }
+
+    @Given("Verify that account logout pop-up is visible")
+    public void verify_that_account_logout_pop_up_is_visible() {
+        String actualValue = logoutPage.logoutVerify.getText();
+        System.out.println(actualValue);
+        String expectedValue = "Account Logout";
+        Assert.assertEquals(expectedValue, actualValue);
+    }
+
+    @Given("The user cliks to My Account dropdown menu and My Account button")
+    public void the_user_cliks_to_my_account_dropdown_menu_and_my_account_button() throws InterruptedException {
+        logoutPage.myAccountDDM.click();
+        logoutPage.myAccountDDM2.click();
+        Thread.sleep(3000);
+        actions.sendKeys(Keys.PAGE_DOWN)
+                .sendKeys(Keys.PAGE_DOWN).perform();
+
+        logoutPage.myAccountPageLogout.click();
+        Thread.sleep(3000);
+
+    }
+
+    @Given("Verify that account logout is visible")
+    public void verify_that_account_logout_is_visible() {
+        String actualValue = logoutPage.myAccountPageLogoutVerify.getText();
+        System.out.println(actualValue);
+        String expectedValue = "Account Logout";
+        Assert.assertEquals(expectedValue, actualValue);
+    }
+
+    @Given("Close driver")
+    public void close_driver() {
+       Driver.driver.close();
+    }
+
+}
---
+++ src/test/resources/features/US02_LogoutFunctiionality.feature
@@ -0,0 +1,32 @@
+Feature: Logout Funcionality
+
+  @logout
+  Scenario Outline: Logout Test
+    * The user is on the home page
+    * The user clicks to My Account
+    * The user should be able to see and click Login button
+    * The user enters correct "<email>" ,"<password>" and clicks black login button
+    * The user should be able to login and see positive login message
+    * The user cliks to My Account dropdown menu and Logout button
+    * Verify that account logout pop-up is visible
+    * Close driver
+
+    Examples:
+      | email          | password     |
+      | ali1@gmail.com | galatasaray. |
+
+  @logout
+  Scenario Outline: Logout Test on My account page
+    * The user is on the home page
+    * The user clicks to My Account
+    * The user should be able to see and click Login button
+    * The user enters correct "<email>" ,"<password>" and clicks black login button
+    * The user should be able to login and see positive login message
+    * The user cliks to My Account dropdown menu and My Account button
+    * Verify that account logout is visible
+    * Close driver
+
+    Examples:
+      | email          | password     |
+      | ali1@gmail.com | galatasaray. |
+
