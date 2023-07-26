2e46518bd963a58c768f08ad3c7c1b61a86bab4d
Fatih <faydan80@outlook.com>
2023-07-26 17:04:53 +0200

    End of the Test Case

--- src/test/java/com/euroTech/pages/CartIcon.java
+++ src/test/java/com/euroTech/pages/CartIcon.java
@@ -1,11 +1,35 @@
 package com.euroTech.pages;
 
 
+import com.euroTech.utilities.BrowserUtils;
+import com.euroTech.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class CartIcon extends BasePage{
+public class CartIcon extends BasePage {
 
     @FindBy(xpath = "//a[text()='eufy C1 Smart Scale White'][1]")
     public WebElement firstProduct;
+
+    @FindBy(xpath = "//*[text()='Your shopping cart is empty!']")
+    public WebElement emptyCart;
+    @FindBy(xpath = "//a[contains(@href,'checkout/cart')]")
+    public WebElement viewCartIcon;
+
+    @FindBy(xpath = "//a[contains(@href,'checkout/checkout')]")
+    public WebElement checkOutButton;
+
+    public void subcategories(String subcategory){
+        Driver.get().findElement(By.xpath("//a[text()='"+subcategory+"']/parent::h4")).click();
+    }
+    public  void addProducts(String products){
+        Driver.get().findElement(By.xpath("//button[contains(@onclick,'"+products+"')][@title='Add to Cart']")).click();
+
+    }
+
+
+
 }
+
--- src/test/java/com/euroTech/stepDefs/US07_CartIconFunctionality_Step_Defs.java
+++ src/test/java/com/euroTech/stepDefs/US07_CartIconFunctionality_Step_Defs.java
@@ -1,31 +1,104 @@
 package com.euroTech.stepDefs;
 
-import com.euroTech.pages.AccountPage;
-import com.euroTech.pages.CartIcon;
-import com.euroTech.pages.HomePage;
-import com.euroTech.pages.LoginPage;
+import com.euroTech.pages.*;
 import com.euroTech.utilities.BrowserUtils;
+import com.euroTech.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class US07_CartIconFunctionality_Step_Defs {
-    HomePage homePage=new HomePage();
+    HomePage homePage = new HomePage();
     LoginPage loginPage = new LoginPage();
-    CartIcon cartIcon=new CartIcon();
-    @When("The user is able to add a {string} to the cart")
-    public void the_user_is_able_to_add_a_to_the_cart(String product) {
+    CartIcon cartIcon = new CartIcon();
+    CheckOutPage checkOutPage = new CheckOutPage();
+    SecurityPage securityPage= new SecurityPage();
+
+
+    @Then("The user is able to see the product added to the cart")
+    public void the_user_is_able_to_see_the_product_added_to_the_cart() {
+        BrowserUtils.verifyElementDisplayed(cartIcon.firstProduct);
+
+    }
+
+    @When("The user is able to add a product to the cart")
+    public void the_user_is_able_to_add_a_to_the_cart() {
         homePage.addProductHomepage.click();
 
     }
+
     @Then("The user is able to click the Cart Icon")
     public void the_user_is_able_to_click_the_cart_icon() {
         homePage.cartIcon.click();
 
     }
-    @Then("The user is able to see the product added to the cart")
-    public void the_user_is_able_to_see_the_product_added_to_the_cart() {
-        BrowserUtils.verifyElementDisplayed(cartIcon.firstProduct);
+
+
+    @Then("The user is able to receive a verify message that there isn't any product in the cart.")
+    public void the_user_is_able_to_receive_a_there_isn_t_any_product_in_the_cart() {
+        String expectedMessage = "Your shopping cart is empty!";
+        Assert.assertEquals(cartIcon.emptyCart.getText(), expectedMessage);
+
+    }
+
+    @Then("The user is able to click the view cart button on the side window")
+    public void the_user_is_able_to_click_the_view_cart_button_on_the_side_window() {
+        cartIcon.viewCartIcon.click();
+        BrowserUtils.waitFor(3);
+    }
+
+    @Then("The user is able to navigate to the shopping cart page")
+    public void the_user_is_able_to_navigate_to_the_shopping_cart_page() {
+        String expectedUrl = "https://dgmarkt.com/index.php?route=checkout/cart";
+        System.out.println("Actual Url: " + Driver.get().getCurrentUrl());
+        Assert.assertEquals(Driver.get().getCurrentUrl(), expectedUrl);
+
+    }
+
+    @Then("The user is able to navigate to {string}")
+    public void the_user_is_able_to_navigate_to(String subcategiories) {
+        cartIcon.subcategories(subcategiories);
+
+    }
+
+    @Then("The user is able to add {string} to the cart")
+    public void the_user_is_able_to_add_to_the_cart(String products) {
+        cartIcon.addProducts(products);
+
+    }
+
+    @And("The user is able to see the check out button")
+    public void the_user_is_able_to_see_the_check_out_button() {
+        BrowserUtils.verifyElementDisplayed(cartIcon.checkOutButton);
+    }
+
+    @Then("The user is able to navigate to check out page by clicking the check out button")
+    public void the_user_is_able_to_navigate_to_check_out_page_by_clicking_the_check_out_button() {
+        cartIcon.checkOutButton.click();
+        String expectedUrl = "https://dgmarkt.com/index.php?route=checkout/checkout";
+        Assert.assertEquals(Driver.get().getCurrentUrl(), expectedUrl);
+    }
+
+    @Then("The user is able to continue to checkout to order the products")
+    public void the_user_is_able_to_continue_to_checkout_to_order_the_products() {
+        checkOutPage.continueButton.click();
+        checkOutPage.continueShippingAddress.click();
+        checkOutPage.continueShippingMethod.click();
+        checkOutPage.checkbox.click();
+        checkOutPage.continuePaymentMethod.click();
+        checkOutPage.confirmButton.click();
+
+        String expectedUrl = "https://dgmarkt.com/index.php?route=checkout/success";
+        Assert.assertEquals(expectedUrl, Driver.get().getCurrentUrl());
+        BrowserUtils.verifyElementDisplayed(checkOutPage.successfulOrderMessage);
+        checkOutPage.continueAfterOrderButton.click();
+        securityPage.newsLetterClosedButton.click();
+        String homePageUrl="https://dgmarkt.com/";
+        Assert.assertEquals(homePageUrl,Driver.get().getCurrentUrl());
+
 
     }
 }
