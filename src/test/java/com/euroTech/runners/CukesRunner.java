46fec655b8fb4507ec9f404f536edf041e827911
Fatih <faydan80@outlook.com>
2023-07-26 22:21:02 +0200

    Was verändert werden

--- src/test/java/com/euroTech/runners/CukesRunner.java
+++ src/test/java/com/euroTech/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/euroTech/stepDefs",
         dryRun = false,
-        tags = "@smoke"
+        tags = "@cart"
 
 )
 
