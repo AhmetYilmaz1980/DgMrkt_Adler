de15aa7a850ba672cca4ee578357ed83419c3353
Ahmet Yilmaz <121608417+AhmetYilmaz1980@users.noreply.github.com>
2023-11-03 20:24:56 +0100

    Update CukesRunner.java

--- src/test/java/com/euroTech/runners/CukesRunner.java
+++ src/test/java/com/euroTech/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/euroTech/stepDefs",
         dryRun = false,
-        tags = "@B7DGMRT-7"
+        tags = "@B7DGMRT-17"
 
 )
 public class CukesRunner {
